1a4d5f7d779425b8390d5d021ac66357150f5a19
GrandFather051 <mr.focker42@gmail.com>
2016-12-09 00:47:28 +0330

    Update sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -239,24 +239,11 @@ function create_config( )
     "time",
     "welcome"
     },
-    sudo_users = {144658773,114934518,94746365},--Sudo users
+    sudo_users = {209218853},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Sbss Bot V2 Beta
-    An Advanced Anti Spam Bot Forked On TeleSeed
-    
-    Develpoed By:
-    @sina7sk
-    Manager:
-    @yellowhat
-    Founder:
-    @amirho3in
-    
-    Special Thank To:
-    Mehr Pouya
-    Arman
-    IM/-\N
-    Creed Is Dead
+    about_text = [[توسط:
+		
     
 ]],
     help_text_realm = [[
@@ -342,10 +329,7 @@ _________________________________________
 ⚠️نیاز نیست از '!' و '/' استفاده کنید*⚠️
 _________________________________________
 _________________________________________
-_________________________________________
-ليست سودوها :@Cia_00_07
-@sina7sk
-@amirho3in
+
 ]]
   }
   serialize_to_file(config, './data/config.lua')
